ee2543639f5d174b070d8fb207ec76c2f0297746
Vaibhav Matere <vpm00718897@gmail.com>
2023-05-04 17:02:15 +0530

    initialize homepage, product-page & error page

---
+++ src/Components/Error.jsx
@@ -0,0 +1,11 @@
+import Layout from "./Layout";
+
+const Error = () => {
+  return (
+    <>
+      <Layout>Error!!! Page not Found</Layout>
+    </>
+  );
+};
+
+export default Error;
---
+++ src/Pages/Homepage.jsx
@@ -0,0 +1,7 @@
+import Layout from "../Components/Layout";
+
+const Homepage = () => {
+  return <Layout>Homepage</Layout>;
+};
+
+export default Homepage;
