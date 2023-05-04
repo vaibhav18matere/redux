614752fcd33bc1593db6d98c62123ad9f615f20d
Vaibhav Matere <vpm00718897@gmail.com>
2023-05-04 17:01:45 +0530

    routing added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
- import "./App.css";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Homepage from "./Pages/Homepage";
+import ProductDetails from "./Pages/ProductDetails";
+import Error from "./Components/Error";
 
 function App() {
   return (
     <>
-      <p>redux</p>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/product/:pid" element={<ProductDetails />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
     </>
   );
 }
