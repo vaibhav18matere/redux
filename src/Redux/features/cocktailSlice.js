104ade32277423553989cd6caa5c09e71b05eebd
Vaibhav Matere <vpm00718897@gmail.com>
2023-05-05 12:47:17 +0530

    feat : single page data shown for cocktail

--- src/Redux/features/cocktailSlice.js
+++ src/Redux/features/cocktailSlice.js
@@ -1,10 +1,23 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
+// for fetching all cocktails on homepage
+
 export const fetchCocktails = createAsyncThunk(
   "cocktails/fetchCocktails",
   async () => {
     return fetch(
       "https://www.thecocktaildb.com/api/json/v1/1/search.php?s="
+    ).then((res) => res.json());
+  }
+);
+
+// for fetching single cocktail data on separate cocktail page
+
+export const fetchSingleCocktail = createAsyncThunk(
+  "cocktails/fetchSingleCocktail",
+  async ({ id }) => {
+    return fetch(
+      `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`
     ).then((res) => res.json());
   }
 );
@@ -17,6 +30,8 @@ const initialState = {
 };
 
 const extraReducers = {
+  // for all cocktails
+
   [fetchCocktails.pending]: (state) => {
     state.loading = true;
   },
@@ -25,6 +40,19 @@ const extraReducers = {
     state.cocktails = action.payload.drinks;
   },
   [fetchCocktails.rejected]: (state, action) => {
+    state.loading = false;
+    state.error = action.payload;
+  },
+
+  // for single cocktail
+  [fetchSingleCocktail.pending]: (state) => {
+    state.loading = true;
+  },
+  [fetchSingleCocktail.fulfilled]: (state, action) => {
+    state.loading = false;
+    state.cocktail = action.payload.drinks;
+  },
+  [fetchSingleCocktail.rejected]: (state, action) => {
     state.loading = false;
     state.error = action.payload;
   },
